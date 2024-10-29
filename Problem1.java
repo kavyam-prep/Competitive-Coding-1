e36da3d6a2d5ae2ac41ac924d64c8a4594728ff1
Kavya Mandaliya <mandaliyak15@gmail.com>
2024-10-28 18:29:13 -0700

    Completed Competitive Coding -1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,34 @@
+import java.util.*;
 
+public class Problem1{
+	public static int findMissingNumber(int nums[]){
+		if (nums[0] != 1) return 1;
+		int low = 0, high = nums.length-1;
+		while(low <= high){
+			if(high-low == 1){
+				return (nums[high]+nums[low])/2;
+			}
+			int mid = low + (high-low)/2;
+			int midDiff = nums[mid] - mid;
+			int leftDiff = nums[low] - low;
+			int rightDiff = nums[high] - high;
+			if(midDiff != leftDiff){
+				high = mid;
+			}else if(midDiff != rightDiff){
+				low = mid;
+			}else{
+				break;
+			}
+		}
+		return nums[nums.length-1]+1;
+	}
+
+	public static void main(String args[]){
+		int nums[] = {2,3,4};
+		int nums1[] = {1,2,3,4,5,6,7,8};
+		int nums2[] = {1,2,3,4,5,6,8};
+		int nums3[] = {1,3,4,5,6,7,8};
+		System.out.println(findMissingNumber(nums3));
+	}
+	
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,96 @@
 
+public class Problem2
+{
+	public static class Heap {
+		int capacity;
+		int size;
+		int heap[];
+		public Heap(int capacity){
+			this.capacity = capacity;
+			this.heap = new int[capacity];
+		}
+		
+		public int getLeftChild(int parent){
+			return 2 * parent;
+		}
+		
+		public int getRightChild(int parent){
+			return 2 * parent + 1;
+		}
+		
+		public int getParent(int child){
+			return child/2;
+		}
+		
+		public void peek(){
+			System.out.println("Peek - " +  heap[1]);
+		}
+		
+		boolean isLeafNode(int i){
+			return (i > size/2 && i <= size);
+		}
+		
+		public void swap(int i, int j){
+			int tmp = heap[i];
+			heap[i] = heap[j];
+			heap[j] = tmp;
+		}
+		
+		public void insert(int element){
+			if(size >= capacity-1){
+				System.out.println("Heap is full");
+				return;
+			}
+			heap[++size] = element;
+			int curr = size;
+			while(heap[curr] < heap[getParent(curr)]){
+				swap(curr, getParent(curr));
+				curr = getParent(curr);
+			}
+		}
+		
+		public void heapify(int i){
+			if(!isLeafNode(i)){
+				int swapPos = i;
+				if(getRightChild(i) <= size){
+					swapPos = heap[getLeftChild(i)] < heap[getRightChild(i)] ? getLeftChild(i) : getRightChild(i);
+				}else{
+					swapPos = getLeftChild(i);
+				}
+				if(heap[i] > heap[getRightChild(i)] || heap[getRightChild(i)] < heap[i]){
+					swap(i, swapPos);
+					heapify(swapPos);
+				}
+			}
+		}
+		
+		public int remove(){
+			int pop = heap[1];
+			heap[1] = heap[size--];
+			heapify(1);
+			return pop;
+		}
+		
+		public void print(){
+			System.out.println("Printing Heap");
+			for(int i: heap){
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String args[]){
+		Heap heap = new Heap(5);
+		heap.insert(1);
+		heap.insert(2);
+		heap.insert(3);
+		heap.insert(4);
+		heap.print();
+		heap.peek();
+		heap.remove();
+		heap.print();
+		heap.insert(5);
+		heap.print();
+	}
+}
